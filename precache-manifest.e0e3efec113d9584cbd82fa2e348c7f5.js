110e6c30f06b73d90f7ea5ad2319451383d6080e
Justin <jwhittington92@yahoo.com>
2018-12-03 12:37:07 -0500

    Updates

---
+++ precache-manifest.e0e3efec113d9584cbd82fa2e348c7f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "794047f1fffdfe5ffde3",
+    "url": "/React-Memory-Game/static/js/runtime~main.794047f1.js"
+  },
+  {
+    "revision": "6c639c6891021b1012e1",
+    "url": "/React-Memory-Game/static/js/main.6c639c68.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/React-Memory-Game/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "6c639c6891021b1012e1",
+    "url": "/React-Memory-Game/static/css/main.1f0b6a57.chunk.css"
+  },
+  {
+    "revision": "b2b3022255a88e1dea5530ce7211df4f",
+    "url": "/React-Memory-Game/index.html"
+  }
+];
